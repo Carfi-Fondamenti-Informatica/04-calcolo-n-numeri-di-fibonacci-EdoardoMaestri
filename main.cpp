191de6eadd73fb1eda8ac78d03c12528d4f99aac
EdoardoMaestri <115471996+EdoardoMaestri@users.noreply.github.com>
2022-11-22 20:51:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main() 
+{
+   int n;
+   cin >> n;
+   if (n>=2)
+    {
+        int a = 1;
+        int b = 1;
+        int c;
+
+      for (int i = 0; i < n; i++)
+      {
+           cout << a << endl;
+           c = a;
+           a = b;
+           b = a + c;
+      }
+    }
+  else
+   {
+        cout << "errore";
+   }
+  return 0;
 }
